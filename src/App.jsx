8cb31025607b05cc04cacbb1d1f5265ceabb52af
Latif Lumi <latiflumi12@gmail.com>
2025-11-24 12:02:52 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import {useState, useEffect } from 'react'
+import CoinCard from './components/CoinCard'
+import LimitSelector from './components/LimitSelector';
+
+const API_URL = import.meta.env.VITE_API_URL;
+
+const App = () => {
+  const [coins, setCoins] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [limit, setLimit] = useState(10);
+
+  useEffect(() => {
+    const fetchCoins = async () => {
+      try {
+        const res = await fetch(`${API_URL}&order=market_cap_desc&per_page=${limit}&page=1&sparkline=false`);
+        if(!res.ok) throw new Error('Failed to fetch data')
+        const data = await res.json();
+      setCoins(data);
+      }
+      catch(err) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    }
+    fetchCoins();
+  }, [limit]);
+  
+  return ( 
+<div>
+  <h1> Crypto Dash </h1>
+  { loading && <p> Loading... </p>}
+  {error && <div className='error'> {error} </div>}
+  <LimitSelector limit={limit} onLimitChange={setLimit} />
+  {!loading && !error && (
+    <main className='grid'>
+    { coins.map((coin)=>(
+      <CoinCard key={coin.id} coin={coin} />
+    ))}
+    </main>
+  )}
+</div>
+   )
+}
+ 
+export default App;
